f47b65c9ccb419dc7eafb9562972449bce5482d6
ZroWcALE <webwork625@proton.me>
2024-07-07 04:42:46 -0400

    ..

--- planner.js
+++ planner.js
@@ -1,8 +1,7 @@
-// end of month //
 function settingDate(date, day) {
   date = new Date(date)
   date.setDate(day)
-  date.setHours(4)
+  date.setHours(23)
   return date
 }
 
@@ -18,8 +17,9 @@ function getDatesBetween(date1, date2) {
   while (date1 <= date2) {
     if (date1.getDate() != 31) {
       temp = settingDate(date1, 0)
+
       if (temp >= range1 && temp <= range2) dates.push(temp)
-      date1 = settingDate(date1, 31)
+      date1 = setDate(date1, 31)
     } else {
       temp = new Date(date1)
       if (temp >= range1 && temp <= range2) dates.push(temp)
@@ -38,12 +38,12 @@ function getDatesBetween(date1, date2) {
     { shortDay: 'Sun', fullDay: 'Sunday' },
   ]
 
+  let LastDate, firstDate
   for (let i = 0; i < dates.length; i++) {
-    lastDate = dates[i]
-    firstDate = dates[i]
+    LastDate = dates[i]
     firstDate = new Date(dates[i].getFullYear(), dates[i].getMonth(), 1)
 
-    content += "<div id='calenderTable_" + (i + 1) + " '> "
+    content += "<div id='calenderTable_" + (i + 1) + "'>"
     content +=
       '<h2> ' +
       firstDate.toString().split(' ')[0] +
@@ -51,44 +51,8 @@ function getDatesBetween(date1, date2) {
       firstDate.getFullYear() +
       '</h2>'
 
-    content += '<table >'
-    content += '<thead>'
-    weekDays.map((item) => {
-      content += '<th>' + item.fullDay + '</th>'
-    })
-
-    content += '</thead>'
-    content += '<tbody>'
-    let j = 1
-    let displayNumber
-    let idMonth
-    while (j <= lastDate.getDate()) {
-      content += '<tr>'
-      for (let k = 0; k < 7; k++) {
-        displayNumber = j < 10 ? '0' + j : j
-        if (j === 1) {
-          if (firstDate.toString().split(' ')[0] === weekDays[k].shortDay) {
-            content += '<td>' + displayNumber + '</td>'
-            j++
-          } else {
-            content += '<td></td>'
-          }
-        } else if (j > lastDate.getDate) {
-          content += '<td></td>'
-        } else {
-          content += '<td>' + displayNumber + '</td>'
-          j++
-        }
-      }
-      content += '</tr>'
-    }
-
-    content += '</tbody>'
-    content += '</table>'
     content += '</div>'
   }
-
-  return content
 }
 
 let content = getDatesBetween('2024/01/01', '2025/01/01')
